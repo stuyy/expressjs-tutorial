b4a91cf56934db3fc68c32a997f87e9edfa1ca3a
Anson <ansfoong@gmail.com>
2022-01-14 20:16:25 -0700

    Episode #2 - GET Request

--- src/index.js
+++ src/index.js
@@ -4,3 +4,20 @@ const app = express();
 const PORT = 3001;
 
 app.listen(PORT, () => console.log(`Running Express Server on Port ${PORT}!`));
+
+app.get('/groceries', (request, response) => {
+  response.send([
+    {
+      item: 'milk',
+      quantity: 2,
+    },
+    {
+      item: 'cereal',
+      quantity: 1,
+    },
+    {
+      item: 'pop-tarts',
+      quantity: 1,
+    },
+  ]);
+});
