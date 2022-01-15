c65ac39572c028b8c2194b8e0f5d36918d97ff01
Anson <ansfoong@gmail.com>
2022-01-14 20:34:50 -0700

    Episode #3 - Post Requests

--- src/index.js
+++ src/index.js
@@ -3,21 +3,32 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.listen(PORT, () => console.log(`Running Express Server on Port ${PORT}!`));
 
+const groceryList = [
+  {
+    item: 'milk',
+    quantity: 2,
+  },
+  {
+    item: 'cereal',
+    quantity: 1,
+  },
+  {
+    item: 'pop-tarts',
+    quantity: 1,
+  },
+];
+
 app.get('/groceries', (request, response) => {
-  response.send([
-    {
-      item: 'milk',
-      quantity: 2,
-    },
-    {
-      item: 'cereal',
-      quantity: 1,
-    },
-    {
-      item: 'pop-tarts',
-      quantity: 1,
-    },
-  ]);
+  response.send(groceryList);
+});
+
+app.post('/groceries', (request, response) => {
+  console.log(request.body);
+  groceryList.push(request.body);
+  response.send(201);
 });
