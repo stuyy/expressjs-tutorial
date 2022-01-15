0b6398d0ed51f00288ae57642ffe8c829fbbedd9
Anson <ansfoong@gmail.com>
2022-01-14 20:52:15 -0700

    Episode #4 - Middleware Functions

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ const PORT = 3001;
 app.use(express.json());
 app.use(express.urlencoded());
 
+app.use((req, res, next) => {
+  console.log(`${req.method}:${req.url}`);
+  next();
+});
+
 app.listen(PORT, () => console.log(`Running Express Server on Port ${PORT}!`));
 
 const groceryList = [
